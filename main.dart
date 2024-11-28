8915c5a37101245e4a174837c27ef2093dabccb7
AyoDano <100164168+AyoDano@users.noreply.github.com>
2024-11-28 15:59:23 +0100

    Code Anpassung

--- main.dart
+++ main.dart
@@ -1,11 +1,17 @@
 void main() {
   int ticketNummer = 47;
-  int reihe = (ticketNummer + 11) ~/ 12;
-  int platz = ticketNummer % 12 == 0 ? 12 : ticketNummer % 12;
-  int preis = (platz % 2 == 0) ? 15 : 12;
+  int reihe = ticketNummer ~/ 12;
+  int platz = ticketNummer % 12;
+
+  int preis;
+    if (platz % 2 == 0) {
+    preis = 15;
+    } else {
+    preis = 12;
+    }
 
   print("TICKET-INFO");
-  print("Reihe: $reihe");
+  print("Reihe: ${reihe+1}");
   print("Platz: $platz");
   print("Preis: ${preis}€");
 }
